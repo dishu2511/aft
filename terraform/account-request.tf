2afc36ab20603cb8a17f23e9b8e2da7dec09aea9
Dinesh Sharma <dinesh.sharma@datacom.co.nz>
2022-11-17 21:28:48 +1300

    creating test and uat account

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -1,6 +1,4 @@
-# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
+# creating dev account
 module "dev_account" {
   source = "./modules/aft-account-request"
 
@@ -19,7 +17,7 @@ module "dev_account" {
   account_tags = {
     "ABC:Owner"       = "dineshsharma2511+aft-dev@gmail.com"
     "ABC:Division"    = "ENT"
-    "ABC:Environment" = "Dev"
+    "ABC:Environment" = "dev"
     "ABC:CostCenter"  = "123456"
     "ABC:Vended"      = "true"
     "ABC:DivCode"     = "102"
@@ -38,4 +36,84 @@ module "dev_account" {
   }
 
   account_customizations_name = "dev-customizations"
+}
+
+# creating uat account
+module "test_account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "dineshsharma2511+aft-test@gmail.com"
+    AccountName  = "test"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "workload"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "dineshsharma2511+aft-test@gmail.com"
+    SSOUserFirstName = "Dinesh"
+    SSOUserLastName  = "Sharma"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "dineshsharma2511+aft-test@gmail.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "test"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Dinesh Sharma"
+    change_reason       = "Commissioning test account"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "test-customizations"
+}
+
+# creating uat account
+module "uat_account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "dineshsharma2511+aft-uat@gmail.com"
+    AccountName  = "uat"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "workload"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "dineshsharma2511+aft-uat@gmail.com"
+    SSOUserFirstName = "Dinesh"
+    SSOUserLastName  = "Sharma"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "dineshsharma2511+aft-uat@gmail.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "uat"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Dinesh Sharma"
+    change_reason       = "Commissioning uat account"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "uat-customizations"
 }
