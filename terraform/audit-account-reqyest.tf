c806af160e2070ca822bef1dc0759adee3159f08
Dinesh Sharma <dinesh.sharma@datacom.co.nz>
2022-11-24 13:49:20 +1300

    enrolling audit account into AFT

---
+++ terraform/audit-account-reqyest.tf
@@ -0,0 +1,39 @@
+# creating audit account
+module "audit_account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "dineshsharma2511+aftaudit@gmail.com"
+    AccountName  = "audit"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "workload"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-bal-bla)"
+    SSOUserEmail     = "dineshsharma2511+aftaudit@gmail.com"
+    SSOUserFirstName = "Dinesh"
+    SSOUserLastName  = "Sharma"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "Dinesh"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "audit"
+    "ABC:CostCenter"  = "3333343422222"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Dinesh Sharma"
+    change_reason       = "Commissioning audit account"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "audit-customizations"
+}
