0af96115df6dad339a272932b6e89fe03a1ddf75
Dinesh Sharma <dinesh.sharma@datacom.co.nz>
2022-11-28 18:43:15 +1300

    testing removing tags

--- terraform/audit-account-request.tf
+++ terraform/audit-account-request.tf
@@ -22,7 +22,7 @@ module "audit_account" {
     "ABC:Vended"      = "true"
     "ABC:DivCode"     = "102"
     "ABC:BUCode"      = "ABC003"
-    "ABC:Project"     = "123456"
+    #"ABC:Project"     = "123456"
   }
 
   change_management_parameters = {
