260936d9ed064c96f1cb91850bdeacb0833b4387
Dinesh Sharma <dinesh.sharma@datacom.co.nz>
2022-12-27 18:17:36 +1300

    new account for uat

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -77,3 +77,43 @@ module "preprod_account" {
 
   account_customizations_name = "preprod-customizations"
 }
+
+# creating uat account
+module "uat_account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "dineshsharma2511+aft-uat@gmail.com"
+    AccountName  = "uat"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "workload"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-bal-bla)"
+    SSOUserEmail     = "dineshsharma2511+aft-uat@gmail.com"
+    SSOUserFirstName = "Dinesh"
+    SSOUserLastName  = "Sharma"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "Dinesh"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "uat"
+    "ABC:CostCenter"  = "12345622343222"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    #"ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Dinesh Sharma"
+    change_reason       = "Commissioning uat account"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "uat-customizations"
+}
